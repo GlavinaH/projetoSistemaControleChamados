540c187277a543da0733bf0f8a1acfc4628f14e9
GlavinaH <henriqueglavina@gmail.com>
2024-03-30 03:24:21 -0300

    Criar, editar e deletar equipamentos.

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 from .auth import auth as auth_blueprint
 from .main import main as main_blueprint
+import psycopg2
+import psycopg2.extras
 
 def create_app():
     app = Flask(__name__)
@@ -20,6 +22,13 @@ def create_app():
 app = create_app()
 
 db = SQLAlchemy(app)
+
+DB_HOST="cb4l59cdg4fg1k.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com"
+DB_NAME="d1le26gb6kv12a"
+DB_USER="ud46diq0e2643u"
+DB_PASS="p857111c9c9e05eb3cd3d6b18fa738a29777ab76760b6812a5079e938f8083589"
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
 class Equipamento(db.Model):
     __tablename__='tb_equipamentos'
@@ -43,8 +52,54 @@ def submit():
     equipamento=Equipamento(marca,modelo,numero_serie)
     db.session.add(equipamento)
     db.session.commit()
+    flash("Equipamento adicionado com sucesso.")
     
-    return render_template('meus_equipamentos.html')
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    s = "SELECT * FROM tb_equipamentos"
+    cur.execute(s)
+    lista_equipamentos=cur.fetchall()
+    return render_template("meus_equipamentos.html", lista_equipamentos=lista_equipamentos)
 
+@app.route('/meus_equipamentos')
+def meus_equipamentos():
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    s = "SELECT * FROM tb_equipamentos ORDER BY id_equip ASC"
+    cur.execute(s)
+    lista_equipamentos=cur.fetchall()
+    return render_template("meus_equipamentos.html", lista_equipamentos=lista_equipamentos)
 
+@app.route('/edit/<id>', methods=['POST','GET'])
+def get_equipamento(id):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute(f'SELECT * FROM tb_equipamentos WHERE id_equip={id}')
+    cur.execute('SELECT * FROM tb_equipamentos ORDER BY id_equip ASC')  
+    data=cur.fetchall()
+    cur.close()
+    print(data[0])
+    return render_template('editar_equipamento.html', equipamento=data[0])
 
+@app.route('/update/<id>', methods=['POST'])
+def update_equip(id):
+    if request.method == 'POST':
+        marca=request.form['marca']
+        modelo=request.form['modelo']
+        numero_serie=request.form['numero_serie']
+        
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute(f'''UPDATE tb_equipamentos
+                        SET marca='{marca}',
+                        modelo='{modelo}',
+                        numero_serie='{numero_serie}'
+                        WHERE id_equip={id}''')
+        cur.execute('SELECT * FROM tb_equipamentos ORDER BY id_equip ASC')       
+        flash("Equipamento atualizado com sucesso.")
+        conn.commit()
+        return redirect(url_for('meus_equipamentos'))
+    
+@app.route('/delete/<id>', methods=['POST','GET'])
+def delete_equip(id):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute(f'DELETE FROM tb_equipamentos WHERE id_equip={id}')
+    conn.commit()
+    flash('Equipamento removido com sucesso')
+    return redirect(url_for('meus_equipamentos'))
